a33d41920076c34387a2907414e7f36a8018fe58
gm_glb <719691148@qq.com>
2019-11-13 22:58:49 +0800

    提交测试

---
+++ gmall-user-web/src/main/java/com/gm/gmall/user/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.gm.gmall.user.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.gm.gmall.bean.UmsMember;
+import com.gm.gmall.bean.UmsMemberReceiveAddress;
+import com.gm.gmall.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Reference
+    UserService userService;
+
+    @RequestMapping("/getReceiveAddressByUserID")
+    public List<UmsMemberReceiveAddress> getReceiveAddressByUserID(String memberId){
+
+        List<UmsMemberReceiveAddress> umsMemberReceiveAddresses = userService.getReceiveAddressByUserID(memberId);
+        return umsMemberReceiveAddresses;
+    }
+
+    @RequestMapping("/getAllUser")
+    public List<UmsMember> getAllUser(){
+        List<UmsMember> umsMembers = userService.getAllUser();
+        return  umsMembers;
+    }
+
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello user";
+    }
+
+}
